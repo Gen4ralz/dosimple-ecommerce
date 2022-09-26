fdcb5c4e30fddd06157872956128c6fd4c556ca2
Gen4ralz <acetyl-coa@windowslive.com>
2022-09-26 14:34:18 +0700

    Add-Admin-Dashboard-logout

--- frontend/src/components/AdminNav.js
+++ frontend/src/components/AdminNav.js
@@ -1,7 +1,12 @@
-import { Link } from 'react-router-dom';
+import { logout } from '../store/reducers/authReducer';
 import { ListBulletIcon } from '@heroicons/react/24/outline';
+import { useDispatch } from 'react-redux';
 
 const AdminNav = ({ openSidebar }) => {
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
   return (
     <nav className="fixed left-0 sm:left-56 top-0 right-0 mx-4">
       <div className="bg-gray-800 w-full flex p-4 justify-between sm:justify-end items-center">
@@ -9,12 +14,12 @@ const AdminNav = ({ openSidebar }) => {
           className="w-8 h-8 text-white cursor-pointer sm:hidden block"
           onClick={openSidebar}
         />
-        <Link
-          to="/"
+        <button
           className="py-2 px-4 bg-indigo-600 text-white rounded-md capitalize"
+          onClick={logoutHandler}
         >
           Logout
-        </Link>
+        </button>
       </div>
     </nav>
   );
