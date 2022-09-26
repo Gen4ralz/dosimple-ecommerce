f20367094de67727a16cff9431e71fd0e1b9d89a
Gen4ralz <acetyl-coa@windowslive.com>
2022-09-27 02:17:58 +0700

    Fetch-Categories-API

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
-const categorySchema = mongoose.Schema({
-  name: {
-    type: String,
-    require: true,
-    unique: true,
+const categorySchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+      unique: true,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model('category', categorySchema);
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -2,12 +2,19 @@ const express = require('express');
 const categoryValidations = require('../validations/categoryValidations');
 const Category = require('../controllers/categoryController');
 const Authorization = require('../services/Authorization');
+const categoryController = require('../controllers/categoryController');
 const router = express.Router();
 
 router.post(
   '/create-category',
   [categoryValidations, Authorization.authorized],
   Category.create
+);
+
+router.get(
+  '/categories/:page',
+  Authorization.authorized,
+  categoryController.categories
 );
 
 module.exports = router;
